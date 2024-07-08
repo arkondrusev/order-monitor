af97cf00d50ee3b523efb24483ba28ae33d0f952
Artem <artem.kondrusev@gmail.com>
2024-07-09 02:15:02 +0300

    - initial Spring commit

---
+++ order-monitor/src/main/java/com/example/ordermonitor/OrderMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordermonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMonitorApplication.class, args);
+	}
+
+}

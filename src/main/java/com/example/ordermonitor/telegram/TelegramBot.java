4e84bf34d40237ed778b72d984913b8c1466e086
Artem <artem.kondrusev@gmail.com>
2024-07-22 04:54:00 +0300

    - bot's sendMessage method example

--- src/main/java/com/example/ordermonitor/telegram/TelegramBot.java
+++ src/main/java/com/example/ordermonitor/telegram/TelegramBot.java
@@ -4,17 +4,23 @@ import com.example.ordermonitor.telegram.config.TelegramConfig;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 @Component
 public class TelegramBot implements LongPollingSingleThreadUpdateConsumer {
 
     private TelegramConfig telegramConfig;
+    private TelegramClient telegramClient;
 
     public TelegramBot(@Autowired TelegramConfig telegramConfig) {
         this.telegramConfig = telegramConfig;
+        this.telegramClient = new OkHttpTelegramClient(telegramConfig.getBotToken());
     }
 
     @Override
@@ -22,6 +28,15 @@ public class TelegramBot implements LongPollingSingleThreadUpdateConsumer {
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
             System.out.println(update.getMessage().getText());
+
+            SendMessage sendMessage = new SendMessage(update.getMessage().getChatId().toString(),
+                    "Received text: " + update.getMessage().getText());
+
+            try {
+                telegramClient.execute(sendMessage);
+            } catch (TelegramApiException e) {
+                System.out.println(e);
+            }
         }
     }
 

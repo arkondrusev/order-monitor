f28bb2d4ceaf8abf99fe7200b2d57926b23e3998
Artem <artem.kondrusev@gmail.com>
2024-08-31 03:41:07 +0300

    - mapper remade to custom (with abstract class)

--- src/main/java/com/example/ordermonitor/mapper/SEOrderWrapper2StockExchangeOrderMapper.java
+++ src/main/java/com/example/ordermonitor/mapper/SEOrderWrapper2StockExchangeOrderMapper.java
@@ -4,32 +4,35 @@ import com.example.ordermonitor.dto.SEOrderWrapper;
 import com.example.ordermonitor.model.StockExchange;
 import com.example.ordermonitor.model.StockExchangeOrder;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @Mapper(config = OrderMonitorMapperConfig.class)
-public interface SEOrderWrapper2StockExchangeOrderMapper {
+public abstract class SEOrderWrapper2StockExchangeOrderMapper {
 
-    SEOrderWrapper2StockExchangeOrderMapper INSTANCE = Mappers.getMapper(SEOrderWrapper2StockExchangeOrderMapper.class);
+    public static SEOrderWrapper2StockExchangeOrderMapper INSTANCE = Mappers
+            .getMapper(SEOrderWrapper2StockExchangeOrderMapper.class);
 
-    @Mapping(target = "id", expression = "java(null)")
-    @Mapping(target = "stockExchange", source = "stockExchange")
-    @Mapping(target = "seOrderId", source = "wrapper.orderId")
-    @Mapping(target = "type", source = "wrapper.orderType")
-    @Mapping(target = "instrument", source = "wrapper.instrument")
-    @Mapping(target = "tradeSide", source = "wrapper.tradeSide")
-    @Mapping(target = "quantity", expression = "java(null)")
-    @Mapping(target = "price", expression = "java(null)")
-    @Mapping(target = "openTimestamp", expression = "java(null)")
-    @Mapping(target = "executeTimestamp", expression = "java(null)")
-    @Mapping(target = "state", source = "wrapper.state")
-    StockExchangeOrder SEOrderWrapper2StockExchangeOrder(SEOrderWrapper wrapper, StockExchange stockExchange);
+    public StockExchangeOrder SEOrderWrapper2StockExchangeOrder(SEOrderWrapper wrapper, StockExchange stockExchange) {
+        StockExchangeOrder stockExchangeOrder = new StockExchangeOrder();
+        stockExchangeOrder.setId(null);
+        stockExchangeOrder.setStockExchange(stockExchange);
+        stockExchangeOrder.setType(wrapper.getOrderType());
+        stockExchangeOrder.setInstrument(wrapper.getInstrument());
+        stockExchangeOrder.setTradeSide(wrapper.getTradeSide());
+        stockExchangeOrder.setQuantity(new BigDecimal(wrapper.getQuantity()));
+        stockExchangeOrder.setPrice(new BigDecimal(wrapper.getPrice()));
+        stockExchangeOrder.setOpenTimestamp(calcZonedDateTime(wrapper.getOpenTimestamp()));
+        stockExchangeOrder.setExecuteTimestamp(null);
+        stockExchangeOrder.setState(wrapper.getState());
+        return stockExchangeOrder;
+    }
 
-    private ZonedDateTime calcZonedDateTime(String epochSecondsString) {
+    private static ZonedDateTime calcZonedDateTime(String epochSecondsString) {
         Long openTimestamp = Long.parseLong(epochSecondsString);
         Instant instantTimestamp = Instant.ofEpochSecond(openTimestamp);
         return ZonedDateTime.ofInstant(instantTimestamp, ZoneId.of("UTC"));

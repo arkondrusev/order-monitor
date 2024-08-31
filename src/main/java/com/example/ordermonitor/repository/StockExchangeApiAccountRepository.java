656eedbaf211b3306a162f9faa8c9e0f137d221d
Artem <artem.kondrusev@gmail.com>
2024-08-31 22:26:57 +0300

    - StockExchangeApiAccount repository

---
+++ src/main/java/com/example/ordermonitor/repository/StockExchangeApiAccountRepository.java
@@ -0,0 +1,8 @@
+package com.example.ordermonitor.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.ordermonitor.model.StockExchangeApiAccount;
+
+public interface StockExchangeApiAccountRepository extends CrudRepository<StockExchangeApiAccount, Long> {
+
+}
